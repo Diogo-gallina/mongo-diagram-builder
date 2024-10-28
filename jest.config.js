312661015b35ad69ef21dac4a40917a1f57c0eaf
Diogo-gallina <diogogallina20@gmail.com>
2024-10-28 14:19:50 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    moduleFileExtensions: ['js', 'json', 'node'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js$',
+};
